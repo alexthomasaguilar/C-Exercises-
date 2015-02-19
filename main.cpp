853e86fc46c1ecbc721eacf0f974bd8f8218bad0
unknown <phucthepolice@gmail.com>
2015-02-19 11:37:17 -0800

    message

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on February 19, 2015, 11:20 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello MavDu";
+    return 0;
+}
+
