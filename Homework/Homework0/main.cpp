a76101ed95ca7ec8c7f36f7496a9cf719593843a
Alex Aguilar <phucthepolice@gmail.com>
2015-02-26 22:17:20 -0800

    adding homework0

---
+++ Homework/Homework0/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: alexaguilar
+ *
+ * Created on February 26, 2015, 10:10 PM
+ */
+
+#include <cstdlib>
+#include <iostream> 
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "I like to eat enchiladas!";
+    return 0;
+}
+
